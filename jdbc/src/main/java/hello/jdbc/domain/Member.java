d97e5a4ab824fc83b3fac4c9fb4f35147dd3f1da
wkdgus357437 <wkdgus357437@naver.com>
2024-01-19 11:41:26 +0900

    jdbc

---
+++ jdbc/src/main/java/hello/jdbc/domain/Member.java
@@ -0,0 +1,26 @@
+package hello.jdbc.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+	private String memberId;
+	private int money;
+	
+	
+	public Member() {
+	
+	}
+	
+	
+	public Member(String memberId, int money) {
+		super();
+		this.memberId = memberId;
+		this.money = money;
+	}
+	
+	
+	
+	
+}
