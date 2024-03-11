4bbf75267c5dfe06226e364efed58b81b569e873
wkdgus357437 <wkdgus357437@naver.com>
2024-03-11 12:51:35 +0900

    board

--- BoardStudy/src/main/java/user/User.java
+++ BoardStudy/src/main/java/user/User.java
@@ -1,5 +1,10 @@
 package user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
 	
 	private String userID;
@@ -8,36 +13,5 @@ public class User {
 	private String userGender;
 	private String userEmail;
 	
-	
-	public String getUserID() {
-		return userID;
-	}
-	public void setUserID(String userID) {
-		this.userID = userID;
-	}
-	public String getUserPassword() {
-		return userPassword;
-	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserGender() {
-		return userGender;
-	}
-	public void setUserGender(String userGender) {
-		this.userGender = userGender;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
 
 }
